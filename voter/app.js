e419b618a1102687333ca0b5d3b9bffafddf9ca8
André <andre.pruessing42@gmail.com>
2023-01-09 20:03:59 +0100

    dev done

--- voter/app.js
+++ voter/app.js
@@ -2,49 +2,50 @@ const app = Vue.createApp({
   data: function () {
     return {
       submissions: submissions, // aus seed.js ( möglich da import in html file vor app.js import)
-      //   totalVotes: 0,
     };
   },
-  // 3 ways for data changes: computed, methods & watch
-  // code wird ausgeführt, wenn
-  // computed: abhängige Daten sich ändern
-  // methods: sich sichtbare daten ändern, d.h. wen ndas template neu gerendert wird
-  // watch: wenn sich die beobachteten daten ändern
-  // einsatz:
-  // computed: Daten welche von anderen abhängen
-  // method:Eventlistener ( click) oder für permanentes ändern von Daten
-  // watcher: für code welcher sich nicht direkt aufs Template auswirkt z.B. http request, localStorage nutzen etv.
   computed: {
     totalVotes() {
       return this.submissions.reduce((totalVotes, submission) => {
         return totalVotes + submission.votes;
       }, 0);
     },
+    sortedSubmissionsByVotes() {
+      return this.submissions.sort((a, b) => {
+        return b.votes - a.votes;
+      });
+    },
   },
+  methods: {},
+});
+
+app.component("submissionListItem", {
+  template: `<div class="d-flex">
+  <div class="d-shrink-0">
+    <img :src="submission.img" alt="submission.title" />
+  </div>
+  <div class="flex-grow-1 px-2">
+    <h5>
+      {{ submission.title}}
+      <span class="float-end text-primary" style="cursor: pointer">
+        <i class="fa fa-chevron-up" @click="upvote"></i><br />
+        <strong>{{ submission.votes}}</strong><br />
+        <i class="fa fa-chevron-down" @click="downvote"></i>
+      </span>
+    </h5>
+    <div v-html="submission.desc"></div>
+    <small>{{ submission.author}}</small>
+  </div>
+</div>`,
+  props: ["submission"],
   methods: {
-    upvote(idx) {
-      this.submissions[idx].votes++;
+    upvote() {
+      this.submission.votes++;
     },
-    downvote(idx) {
-      this.submissions[idx].votes--;
+    downvote() {
+      this.submission.votes--;
     },
-  },
-  watch: {
-    // submissions(newVal, oldVal){
-    //     console.log(newVal);
-    //     console.log(oldVal);
-    // }
-    // submissions: {
-    //   handler(newVal, oldVal) {
-    //     this.totalVotes = this.submissions.reduce((totalVotes, submission) => {
-    //       return totalVotes + submission.votes;
-    //     }, 0);
-    //   },
-    //   deep: true,
-    //   immediate: true,
-    // },
   },
 });
 
 const vm = app.mount("#app");
-console.log(submissions);
