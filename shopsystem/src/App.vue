6aa81fa040f22d3ff061a9aeeb5bdaa6ac220a7d
André <andre.pruessing42@gmail.com>
2023-07-10 18:48:26 +0200

    clean up router

--- shopsystem/src/App.vue
+++ shopsystem/src/App.vue
@@ -1,22 +1,13 @@
 <template>
   <!-- <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/> -->
-  <HomePage/>
-
+  </nav> -->
+  <router-view/>
 </template>
 
 <script>
-/* Component */
-import HomePage from "@/pages/HomePage.vue";
-
 export default {
   name:"App",
-  components:{
-    HomePage,
-  }
 }
 </script>
 
